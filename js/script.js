5e72ab7640a8ffcfdaa7c419340464c1e2824c01
Retro-Zero <arian.karimion@gmail.com>
2022-06-29 16:28:35 +0430

    responsible

--- js/script.js
+++ js/script.js
@@ -5,3 +5,12 @@ function addToCart() {
     cart_number++;
     cart.textContent = cart_number;
 }
+
+function openNavbar() {
+    let menu_icon = document.getElementById('menu-icon').classList;
+    let sidebar = document.getElementById('sidebar').classList;
+    menu_icon.toggle('open');
+    menu_icon.toggle('fa-times');
+    menu_icon.toggle('fa-bars');
+    sidebar.toggle('open');
+}
